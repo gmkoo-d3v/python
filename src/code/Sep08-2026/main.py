d2c7a3acc0a3953f179cf0d7a4c962aff6c80fdd
gmk <227376749+gmkoo-d3v@users.noreply.github.com>
2026-09-08 17:19:25 +0900

    py day 1

---
+++ src/code/Sep08-2026/main.py
@@ -0,0 +1,34 @@
+# print("hello world")
+
+# x 변수 선언
+
+x = 9999
+x = x+1
+print(x)
+
+# 변수 선언 2
+
+x = 1
+y = 2
+print(x,y)
+print(x+y)
+
+x,y = 3,4
+x,y = y,x # swap
+print(x,y)
+
+# 변수의 작명법: 영문 소문자, 대문자, 숫자, _ (underscore) 사용 가능, 단, 숫자로 시작하면 안된다.
+#  변수 작명 권장 사항: 변수의 이름만 보고도 용도를 알 수 있도록 해야된다.
+asdfesf23e = 10
+print(asdfesf23e)
+
+is_sunny = True
+print("날씨 어떠니?", is_sunny)
+
+my_age =20
+print("my age is", my_age,".")
+
+## 변수는 한글로도 만들수 있습니다. 단, 하지마세요
+# 변수1 =11
+# print(변수1)
+
